185db4f120a72e906f04ff065406eda8410230f7
flixbeat <flixbeat101@gmail.com>
2021-06-15 18:35:00 +0800

    fix infinite loop bug

--- AStar.gd
+++ AStar.gd
@@ -1,49 +1,56 @@
 extends Node2D
 
-onready var tilemap = $TileMap
+onready var _tilemap = $TileMap
 onready var start_point = $StartPoint
 onready var tween = $StartPoint/Tween
-onready var end_point = $EndPoint
 
 var _tileset : TileSet
 var _cur_pos : Vector2
 
 func _ready():
-	_cur_pos = start_point.position
-	var path = _get_path()
-	_move_along_path(path)
+	_tileset = _tilemap.tile_set
 
-func _move_along_path(path):
+func _input(event):
+	if event is InputEventMouseButton and event.is_pressed():
+		var path = _get_path(start_point.position, get_global_mouse_position())
+		_move_along_path(start_point, path)
+
+func _move_along_path(object, path):
+	if path == null:
+		print("there's no way there")
+		return
+	
 	for p in path:
-		var next_pos = tilemap.map_to_world(p.position)
-		next_pos.x += tilemap.cell_size.x / 2
-		next_pos.y += tilemap.cell_size.y / 2
-		tween.interpolate_property(start_point,"position",_cur_pos,next_pos,0.5,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
+		var next_pos = _tilemap.map_to_world(p.position)
+		next_pos.x += _tilemap.cell_size.x / 2
+		next_pos.y += _tilemap.cell_size.y / 2
+		tween.interpolate_property(object,"position",_cur_pos,next_pos,0.1,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
 		tween.start()
 		_cur_pos = next_pos
 		yield(tween,"tween_all_completed")
 # A*
-func _get_path():
+func _get_path(start_position: Vector2, target_position: Vector2):	
+	_cur_pos = start_position
+	
 	var open = [] # Cell[]
 	var closed = [] # Cell[]
 
-	_tileset = tilemap.tile_set
-	var start_coord = tilemap.world_to_map(start_point.position)
-	var end_coord = tilemap.world_to_map(end_point.position)
-	
+	var start_coord = _tilemap.world_to_map(start_position)
+	var end_coord = _tilemap.world_to_map(target_position)
+
 	# put starting node to open
 	open.append(Cell.new(start_coord,0,0))
 	
 	# while there's an open cell to explore
 	while not open.empty():
-		
 		# find least f from open list
 		var q = Cell.new(Vector2.ZERO,INF,INF)
 		for cell in open:
 			if cell.get_f() < q.get_f():
 				q = cell
 		
-		open.remove(open.find(q))
+		
+		open.erase(q)
 		closed.append(q)
 		
 		# if goal was reached
@@ -53,47 +60,64 @@ func _get_path():
 			# get path
 			while current != null:
 				path.append(current)
+				#_tilemap.set_cell(current.position.x, current.position.y,-1)
 				current = current.parent
 			
 			path.invert()
 			return path
+			break
 		
 		# get neighbors
 		var neighbors = _get_neighbors(q.position.x,q.position.y)
-		var successors = [] # Cell
 		
 		# generate successor and set their parents to q
 		for n in neighbors:
-			var tile_index = tilemap.get_cell(n.x,n.y)
-			var is_walkable = _tileset.tile_get_name(tile_index) == "walkable"
-			
-			if not is_walkable:
-				continue
-			
 			var g = q.g + q.position.distance_to(n)
-			var h = n.distance_to(end_point.position)
-			var cell = Cell.new(n,g,h)
+			var h = n.distance_to(target_position)
+			var neighbor = Cell.new(n,g,h)
+			neighbor.parent = q
 			
-			# if in the closed list skip
-			if closed.find(cell) != -1:
-				continue
+			# check if not in the close list
+			var in_close = false
+			for c in closed:
+				if neighbor.position == c.position:
+					in_close = true
 			
-			# check if it's not in the open list
-			var in_open_id = open.find(cell)
-			if in_open_id == -1:
-				cell.parent = q
-				open.append(cell)
-			else:
-				var in_open = open[in_open_id]
-				if cell.g < in_open.g:
-					in_open.parent = cell
-					in_open.g = cell.g + cell.position.direction_to(in_open.position)
-					in_open.h = cell.position.distance_to(end_point.position)
-					
+			# if not in the closed list
+			if not in_close:
+				var is_in_open = false
+				var in_open: Cell
+				
+				# check if in the open list
+				for o in open:
+					if neighbor.position == o.position:
+						in_open = o
+						is_in_open = true
+				
+				# if not in the closed list
+				if not is_in_open:
+					open.append(neighbor)
+					#_tilemap.set_cell(neighbor.position.x, neighbor.position.y, 18)
+				else:
+					var open_neighbor = in_open
+					if neighbor.g < open_neighbor.g:
+						open_neighbor.g = neighbor.g
+						open_neighbor.parent = neighbor.parent
+	return null
+
+func _get_neighbors(x: int, y: int): # Vector2 array	
+	var neighbors = [Vector2(x,y-1),Vector2(x,y+1),Vector2(x-1,y),Vector2(x+1,y)]
 	
-func _get_neighbors(x: int, y: int): # Vector2 array
-	# top, bottom, left, right
-	return [Vector2(x,y-1),Vector2(x,y+1),Vector2(x-1,y),Vector2(x+1,y)]
+	var walkable_neighbors = []
+	
+	for n in neighbors:
+		var tile_index = _tilemap.get_cell(n.x,n.y)
+		var is_walkable = _tileset.tile_get_name(tile_index) == "walkable"
+		
+		if is_walkable:
+			walkable_neighbors.append(n)
+			
+	return walkable_neighbors
 
 class Cell:
 	var parent : Cell
@@ -109,3 +133,4 @@ class Cell:
 		
 	func get_f() -> float:
 		return self.g + self.h
+
